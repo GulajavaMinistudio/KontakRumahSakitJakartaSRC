b04b49f71833d3794a15d7c938e20851a21d7098
Rian YW <darkfirenze@gmail.com>
2017-03-26 19:30:42 +0700

    Ganti apikey

--- src/app/kontak-akses/kontak-data-akses.ts
+++ src/app/kontak-akses/kontak-data-akses.ts
@@ -2,7 +2,7 @@
  * Created by kucingmint on 3/20/17.
  */
 
-export const DATA_AKSES_API = '8Vu5R9+GYrK2MRkPml7CasOQ7i3yW1YPX81cnvVITjvfTDU13gNIBRTeixUUvi+BIovLhPrpEdmNxuHCb2iWTQ==';
+export const DATA_AKSES_API = 'Untuk mendapatkan api key, silahkan mendaftar akun di http://api.jakarta.go.id/';
 
 // error mixed content jika dijalankan dari hosting yang sudah https
 // export const BASE_URLS = 'http//api.jakarta.go.id/v1';
